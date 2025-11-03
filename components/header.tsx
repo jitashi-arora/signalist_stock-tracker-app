7eea0ef7fd23565b87c940fc3c69d0d02640b292
Jitashi Arora <jitashi.arora15@gmail.com>
2025-11-03 18:29:51 +0530

    Implement stock details page

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt ="Signalist logo" width={140} height={32} className="h-8 w-auto curson-pointer"/>
+                    <Image src="/assets/icons/logo.svg" alt ="Stockify logo" width={140} height={32} className="h-8 w-auto curson-pointer"/>
                 </Link>
                 <nav className="hidden sm:block">
                     <NavItems/>
--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -15,10 +15,10 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
         .replace('{{intro}}', intro);
 
     const mailOptions = {
-        from: `"Signalist" <signalist@gamil.com>`,
+        from: `"Stockify" <Stockify@gamil.com>`,
         to: email,
-        subject: `Welcome to Signalist - your stock market toolkit is ready!`,
-        text: 'Thanks for joining Signalist',
+        subject: `Welcome to Stockify - your stock market toolkit is ready!`,
+        text: 'Thanks for joining Stockify',
         html: htmlTemplate,
     }
 
